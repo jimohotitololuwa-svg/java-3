c2eb5e1070bf9828b6ccc071b514d873061073d8
jimohotitololuwa-svg <jimohotitololuwa@gmail.com>
2026-05-10 17:58:33 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+public class Main {
+public static void main(String[] args){
+    byte a = 10;
+    short b = 10000;
+    int c = 125000;
+    long d = 92233720;
+    float e = 43.2225f;
+    double f = 43.141592653589793d;
+    char g = 'm'; //stores one 
+    boolean h = true; //stores true or false
+
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a/b);
+    System.out.println(a%b);
+
+    byte num = 5;
+    System.out.println(num++);
+    System.out.println(++num);
+    Maggie();
+
+
+}
+    public static void Maggie(){
+        System.out.println("Java is fun");
+    }
+}
